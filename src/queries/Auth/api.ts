0003b43e5dae824401a041682bd89a9c0ef64e6a
Huu Tuan Nguyen <nhtuan010302@gmail.com>
2024-10-19 15:25:20 +0700

    update API login

--- src/queries/Auth/api.ts
+++ src/queries/Auth/api.ts
@@ -2,5 +2,5 @@ import { httpService } from "@/configs/httpService";
 import { LoginPayload } from "@/queries/Auth/types";
 
 export const login = (payload: LoginPayload) => {
-  return httpService.post("/login", payload);
+  return httpService.post("/auth/admin/login", payload);
 };
