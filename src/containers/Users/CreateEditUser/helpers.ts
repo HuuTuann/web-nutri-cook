fa0382eba7cfdcad8eadde8c861a74c3b54edfc9
Huu Tuan Nguyen <nhtuan010302@gmail.com>
2024-10-22 20:37:37 +0700

    feat: add query createUser and update API getAllUser

--- src/containers/Users/CreateEditUser/helpers.ts
+++ src/containers/Users/CreateEditUser/helpers.ts
@@ -1,19 +1,22 @@
-import { UsersKey, UserPayload } from "@/queries";
+import { UsersKey, UserPayload, UserGender } from "@/queries";
 import { z } from "zod";
 
 export const userSchema = z.object({
   [UsersKey.USERNAME]: z.string().nonempty({
     message: "Username is required",
   }),
+  [UsersKey.PASSWORD]: z.string().nonempty({
+    message: "Password is required",
+  }),
   [UsersKey.EMAIL]: z.string().email(),
   [UsersKey.FULL_NAME]: z.string().nonempty({
     message: "Full name is required",
   }),
   [UsersKey.AGE]: z.number().int().positive(),
-  [UsersKey.GENDER]: z.string().nonempty({}),
+  [UsersKey.GENDER]: z.number().int(),
   [UsersKey.WEIGHT]: z.number().int().positive(),
   [UsersKey.HEIGHT]: z.number().int().positive(),
-  [UsersKey.GOAL]: z.string().nonempty({}),
+  [UsersKey.GOAL]: z.string(),
 });
 
 export const defaultValues: UserPayload = {
@@ -22,9 +25,20 @@ export const defaultValues: UserPayload = {
   [UsersKey.EMAIL]: "",
   [UsersKey.FULL_NAME]: "",
   [UsersKey.AGE]: 0,
-  [UsersKey.GENDER]: "",
+  [UsersKey.GENDER]: 0,
   [UsersKey.WEIGHT]: 0,
   [UsersKey.HEIGHT]: 0,
   [UsersKey.GOAL]: "",
   [UsersKey.ROLE]: "user",
 };
+
+export const genderOptions = [
+  {
+    label: "Male",
+    value: UserGender.MALE,
+  },
+  {
+    label: "Female",
+    value: UserGender.FEMALE,
+  },
+];
