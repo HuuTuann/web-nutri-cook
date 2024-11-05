9f6c9abca0a98c9cecbf11a65d8b2c15cd0abd56
Huu Tuan Nguyen <nhtuan010302@gmail.com>
2024-11-05 11:55:01 +0700

    feat(CS02-04): update .env

--- src/queries/Auth/api.ts
+++ src/queries/Auth/api.ts
@@ -1,4 +1,4 @@
-import { httpService } from "@/configs/httpService";
+import { httpService } from "@/modules/web-feature-shared";
 import { LoginPayload } from "@/queries/Auth/types";
 
 export const login = (payload: LoginPayload) => {
