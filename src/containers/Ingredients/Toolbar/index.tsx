0c593f0973453473d915bf4de3cbd69259d913ca
Huu Tuan Nguyen <nhtuan010302@gmail.com>
2024-12-13 16:16:12 +0700

    feat(CS02-09): add search ingredient and recipe

--- src/containers/Ingredients/Toolbar/index.tsx
+++ src/containers/Ingredients/Toolbar/index.tsx
@@ -2,21 +2,52 @@ import { Flex, Tooltip } from "antd";
 import { CreateEditIngredient } from "../CreateEditIngredient";
 import { useGetAllIngredient } from "@/queries";
 import { SyncOutlined } from "@ant-design/icons";
-import { Button } from "@/modules/web-feature-shared";
+import { Button, TableParams, useDebounce } from "@/modules/web-feature-shared";
+import { Input } from "antd";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
-export const Toolbar = () => {
+type Props = {
+  ingredientParams: TableParams;
+  setIngredientParams: Dispatch<SetStateAction<TableParams>>;
+};
+export const Toolbar: React.FC<Props> = ({
+  ingredientParams,
+  setIngredientParams,
+}) => {
   const { handleInvalidateIngredient } = useGetAllIngredient();
 
+  const [search, setSearch] = useState<string>("");
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const debouncedSearch = useDebounce(search, 500);
+
+  useEffect(() => {
+    setIngredientParams({
+      ...ingredientParams,
+      search: debouncedSearch,
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedSearch]);
+
   return (
-    <Flex justify="end" gap={8}>
-      <Tooltip title="Refresh">
-        <Button
-          type="default"
-          icon={<SyncOutlined />}
-          onClick={handleInvalidateIngredient}
-        />
-      </Tooltip>
-      <CreateEditIngredient content={<Button>Create Ingredient</Button>} />
+    <Flex justify="space-between">
+      <Input
+        placeholder="Search ingredient by name"
+        className="w-96"
+        onChange={handleSearch}
+      />
+      <Flex justify="end" gap={8}>
+        <Tooltip title="Refresh">
+          <Button
+            type="default"
+            icon={<SyncOutlined />}
+            onClick={handleInvalidateIngredient}
+          />
+        </Tooltip>
+        <CreateEditIngredient content={<Button>Create Ingredient</Button>} />
+      </Flex>
     </Flex>
   );
 };
