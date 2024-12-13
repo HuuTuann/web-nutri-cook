10be673d3ee42f60ef8de339e16e04ed802e119b
Huu Tuan Nguyen <nhtuan010302@gmail.com>
2024-12-13 16:16:12 +0700

    feat(CS02-09): add search ingredient and recipe

--- src/containers/Recipes/Toolbar/index.tsx
+++ src/containers/Recipes/Toolbar/index.tsx
@@ -1,22 +1,50 @@
-import { Flex, Tooltip } from "antd";
+import { Flex, Input, Tooltip } from "antd";
 import { CreateEditRecipe } from "../CreateEditRecipe";
-import { Button } from "@/modules/web-feature-shared";
+import { Button, TableParams, useDebounce } from "@/modules/web-feature-shared";
 import { SyncOutlined } from "@ant-design/icons";
 import { useGetAllRecipe } from "@/queries";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
-export const Toolbar = () => {
+type Props = {
+  recipeParams: TableParams;
+  setRecipeParams: Dispatch<SetStateAction<TableParams>>;
+};
+
+export const Toolbar: React.FC<Props> = ({ recipeParams, setRecipeParams }) => {
   const { handleInvalidateRecipe } = useGetAllRecipe();
 
+  const [search, setSearch] = useState<string>("");
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const debouncedSearch = useDebounce(search, 500);
+
+  useEffect(() => {
+    setRecipeParams({
+      ...recipeParams,
+      search: debouncedSearch,
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedSearch]);
+
   return (
-    <Flex justify="end" gap={8}>
-      <Tooltip title="Refresh">
-        <Button
-          type="default"
-          icon={<SyncOutlined />}
-          onClick={handleInvalidateRecipe}
-        />
-      </Tooltip>
-      <CreateEditRecipe content={<Button>Create Recipe</Button>} />
+    <Flex justify="space-between">
+      <Input
+        placeholder="Search ingredient by name"
+        className="w-96"
+        onChange={handleSearch}
+      />
+      <Flex justify="end" gap={8}>
+        <Tooltip title="Refresh">
+          <Button
+            type="default"
+            icon={<SyncOutlined />}
+            onClick={handleInvalidateRecipe}
+          />
+        </Tooltip>
+        <CreateEditRecipe content={<Button>Create Recipe</Button>} />
+      </Flex>
     </Flex>
   );
 };
